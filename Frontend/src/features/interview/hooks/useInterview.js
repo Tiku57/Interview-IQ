7cc8219eeca3bf68bf80eae4c28db164a176a080
AadityaSattawan <aaditya.sattawan@gmail.com>
2026-05-15 10:47:47 +0530

    fix: make layout children transparent to show unified border, fix getReports empty array fallback

--- Frontend/src/features/interview/hooks/useInterview.js
+++ Frontend/src/features/interview/hooks/useInterview.js
@@ -57,7 +57,7 @@ export const useInterview = () => {
             setLoading(false)
         }
 
-        return response.interviewReports
+        return response ? response.interviewReports : [];
     }
 
     const getResumePdf = async (interviewReportId) => {
