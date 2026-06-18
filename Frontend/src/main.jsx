c43517c3f785ad91bfe72532deaf3f30ca43db20
AadityaSattawan <aaditya.sattawan@gmail.com>
2026-06-18 16:21:02 +0530

    chore: add build marker

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "./style.scss"
 
+console.log("BUILD_VERSION_2026_06_18_FIX_1");
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
